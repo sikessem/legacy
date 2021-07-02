56dbfa1131cbcef6898d455eaf4fcf46e5fc848a
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-03 01:53:11 +0200

    Rename "tpl" directory to "doc"

--- main.php
+++ main.php
@@ -5,7 +5,7 @@ use SIKessEm\Organizer\SystemInterface as System;
 function main(System $sys) {
     $settings = $sys->import('cfg.program');
     extract($settings);
-    $content = $sys->save('tpl.home', $settings);
+    $content = $sys->save('doc.home', $settings);
     echo $content;
     exit;
 }
