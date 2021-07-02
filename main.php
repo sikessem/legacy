8fbf127d8b6b9399e6198e05b5aae753622ef71e
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-03 01:09:20 +0200

    Create the home template

--- main.php
+++ main.php
@@ -5,6 +5,7 @@ use SIKessEm\Organizer\SystemInterface as System;
 function main(System $sys) {
     $settings = $sys->import('cfg.program');
     extract($settings);
-    echo 'Welcome to <a href="'. $home_url .'" title="Go to home">'. $app_name .'</a> !';
+    $content = $sys->save('tpl.home', $settings);
+    echo $content;
     exit;
 }
---
+++ tpl/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" id="ske-doc" class="no-js">
+  <head>
+    <meta charset="UTF-8"/>
+    <base href="/"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>SIKessEm (SIGUI Kessé Emmanuel)</title>
+    <meta name="description" content="SIKessEm (SIGUI Kessé Emmanuel) develops full-time cross-platform programs and releases them using Git. SIKessEm repositories are available on Github. Discover them on its website."/>
+    <link rel="fluid-icon" href="https://sikessem.com/icon.png" title="SIKessEm"/>
+    <link rel="mask-icon" href="https://sikessem.com/logo.svg"/>
+    <link rel="alternate icon" type="image/png" href="https://sikessem.com/favicon.png"/>
+    <link rel="icon" type="image/x-icon" href="https://sikessem.com/favicon.ico"/>
+  </head>
+  <body>
+    <p>Welcome to <a href="<?= $home_url ?>" title="Go to home"><?= $app_name ?></a> !</p>
+  </body>
+</html>
