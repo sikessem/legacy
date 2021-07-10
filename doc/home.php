675018e677d1d913858d64cfc9262d9af4055ffc
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-10 21:59:33 +0200

    Add the home document style

--- doc/home.php
+++ doc/home.php
@@ -21,27 +21,26 @@ $app_author = $app_name.' ('. $app_owner . ')';
   </head>
   <body class="view" onLoad="main();">
     <noscript>Javascript is required at <?= $app_name ?>. Please activate it to continue.</noscript>
-    <div id="MainWrapper">
-      <div class="main layout">
+    <div id="MainWrapper" class="box centered">
+      <div class="main content centered">
         <div class="heading">
-          <header class="box">
-            <h1><a href="home" class="logo" title="<?= $app_name ?> home"><?= $app_name ?></a></h1>
-            <p><a href="menu" class="icon" title="<?= $app_name ?> menu">Menu</a></p>
+          <header>
+            <h1><a href="https://omninf.com/SIKessEm/" class="logo" title="<?= $app_name ?> home"><?= $app_name ?></a></h1>
           </header>
         </div>
-        <div class="content">
-          <main class="home box">
+        <div class="container">
+          <main class="home">
             <div class="heading">
-              <h1 class="heading">Get your application cheaply on time</h1>
+              <h1>Get your application cheaply on time</h1>
             </div>
             <div class="content">
               <div class="subtitle">
                 <p><?= $app_author ?> is a self-taught developer of websites, smartphone apps and cross-plateform software.</p>
               </div>
               <div class="items">
-                <ul class="buttons list">
-                  <li class="item"><a href="about" class="button" title="All about <?= $app_owner?>">All about <?= $app_name?></a></li>
-                  <li class="item"><a href="about" class="button" title="Contact <?= $app_owner?>">Contact <?= $app_name?></a></li>
+                <ul class="buttons list .w centered sa">
+                  <li class="item"><a href="https://omninf.com/SIKessEm/profile/" class="button dark" title="All about <?= $app_owner?>">All about <?= $app_name?></a></li>
+                  <li class="item"><a href="https://omninf.com/SIKessEm/contact/" class="button light" title="Contact <?= $app_owner?>">Contact <?= $app_name?></a></li>
                 </ul>
               </div>
             </div>
@@ -50,9 +49,9 @@ $app_author = $app_name.' ('. $app_owner . ')';
         <div class="footing">
           <footer>
             <nav class="items">
-              <ul class="links list">
-                <li class="item"><a href="space" class="link" title="Go to <?= $app_name ?> spaces"><?= $app_name ?> spaces</a></li>
-                <li class="item"><a href="space" class="link" title="Go to <?= $app_name ?> places"><?= $app_name ?> places</a></li>
+              <ul class="links list centered sb">
+                <li class="item"><a href="https://omninf.com/SIKessEm/spaces/" class="link" title="Go to <?= $app_name ?> spaces"><?= $app_name ?> spaces</a></li>
+                <li class="item"><a href="https://omninf.com/SIKessEm/places/" class="link" title="Go to <?= $app_name ?> places"><?= $app_name ?> places</a></li>
               </ul>
             </nav>
           </footer>
