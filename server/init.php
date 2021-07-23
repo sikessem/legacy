e364d4371520760cb54c19bf54780e8d19368d86
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-23 07:38:02 +0200

    Add templates paths functions

--- server/init.php
+++ server/init.php
@@ -5,6 +5,36 @@ define('CLIENT_DIR', ROOT_DIR . 'client' . DIRECTORY_SEPARATOR);
 
 define('SERVER_DIR', ROOT_DIR . 'server' . DIRECTORY_SEPARATOR);
 
+/**
+ * Get the path of a template
+ *
+ * @param  string $name The template name
+ * @return string       The template path
+ */
+function tpl(string $name): string {
+  return SERVER_DIR . 'tpl' . DIRECTORY_SEPARATOR . $name . '.php';
+}
+
+/**
+ * Get the path of a layout
+ *
+ * @param  string $name The layout name
+ * @return string       The layout path
+ */
+function layout(string $name) {
+  return tpl('layouts' . DIRECTORY_SEPARATOR . $name);
+}
+
+/**
+ * Get the path of a widget
+ *
+ * @param  string $name The widget name
+ * @return string       The widget path
+ */
+function widget(string $name) {
+  return tpl('widgets' . DIRECTORY_SEPARATOR . $name);
+}
+
 require_once ROOT_DIR . 'vendor/autoload.php';
 
 return (function(){
