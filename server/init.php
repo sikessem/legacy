1bb06cd328dcf0b6d63a53e5218cdf816680b4a6
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-23 07:16:34 +0200

    Add basic directories constants

--- server/init.php
+++ server/init.php
@@ -1,6 +1,11 @@
 <?php
+define('ROOT_DIR', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+define('CLIENT_DIR', ROOT_DIR . 'client' . DIRECTORY_SEPARATOR);
+
+define('SERVER_DIR', ROOT_DIR . 'server' . DIRECTORY_SEPARATOR);
+
+require_once ROOT_DIR . 'vendor/autoload.php';
 
 return (function(){
 $opts = (array) @include __DIR__ . '/cfg/program.php';
