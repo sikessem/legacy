32f757e5d9b3ffd11fc178ddf30331bc819fded4
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-23 07:38:28 +0200

    Use templates files functions

--- server/src/cgi/home.php
+++ server/src/cgi/home.php
@@ -3,10 +3,10 @@ $doc_title = $app_author;
 $doc_description = $app_author . 'develops full-time cross-platform programs and releases them using Git. '. $app_name .' repositories are available on Github. Discover them on its website.';
 
 ob_start();
-require SERVER_DIR . 'tpl/widgets/home.view.php';
+require widget('home.view');
 $view = ob_get_clean();
 
 ob_start();
-require SERVER_DIR . 'tpl/layouts/main.render.php';
+require layout('main.render');
 $render = ob_get_clean();
 exit($render);
