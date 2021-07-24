aee24d2036b3289df6dfae9a33355ae3cdee2602
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-24 20:11:58 +0200

    Add a space between app name and description

--- server/src/cgi/home.php
+++ server/src/cgi/home.php
@@ -1,6 +1,6 @@
 <?php
 $doc_title = $app_author;
-$doc_description = $app_author . 'develops full-time cross-platform programs and releases them using Git. '. $app_name .' repositories are available on Github. Discover them on its website.';
+$doc_description = $app_author . ' develops full-time cross-platform programs and releases them using Git. '. $app_name .' repositories are available on Github. Discover them on its website.';
 
 ob_start();
 require widget('home.view');
