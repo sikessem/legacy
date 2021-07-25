60485c2303c7e292e4a872fbdcfe1b106d3989eb
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-25 07:12:43 +0200

    Update the footer style : Add definitions title style

--- server/src/gui/widgets/main.footer.php
+++ server/src/gui/widgets/main.footer.php
@@ -1,33 +1,33 @@
 <footer class="footer">
   <p class="content centered"><a href="home"><img src="icon.png" alt="icon" width="48px" height="48px" style="padding:4px;border-radius:50%;object-fit:cover;"/></a></p>
   <nav class="items">
-    <dl class="list row">
+    <dl class="list box">
       <div class="column">
-        <dt>About Me</dt>
-        <dd>
+        <dt>About Me<span class="underline"></span></dt>
+        <dd class="about">
           <p><?= $author_fullname ?> is a self-taught developer of websites, smartphone apps and cross-plateform software.</p>
           <p><a href="about">All about <?= $author_shortname ?></a></p>
         <dd>
       </div>
       <div class="column">
-        <dt>Follow Me</dt>
+        <dt>Follow Me<span class="underline"></span></dt>
         <dd>
-          <ul>
-            <li><a href="<?= $author_github ?>">Github</a></li>
-            <li><a href="<?= $author_packagist ?>">Packagist</a></li>
-            <li><a href="<?= $author_npmjs ?>">NPM</a></li>
-            <li><a href="<?= $author_linkedin ?>">LinkedIn</a></li>
-            <li><a href="<?= $author_twitter ?>">Twitter</a></li>
-            <li><a href="<?= $author_whatsapp ?>">WhatsApp</a></li>
-            <li><a href="<?= $author_youtube ?>">Youtube</a></li>
-            <li><a href="<?= $author_facebook ?>">Facebook</a></li>
-            <li><a href="<?= $author_instagram ?>">Instagram</a></li>
-            <li><a href="<?= $author_pinterest ?>">Pinterest</a></li>
+          <ul class="list">
+            <li class="item"><a class="link" href="<?= $author_github ?>">Github</a></li>
+            <li class="item"><a class="link" href="<?= $author_packagist ?>">Packagist</a></li>
+            <li class="item"><a class="link" href="<?= $author_npmjs ?>">NPM</a></li>
+            <li class="item"><a class="link" href="<?= $author_linkedin ?>">LinkedIn</a></li>
+            <li class="item"><a class="link" href="<?= $author_twitter ?>">Twitter</a></li>
+            <li class="item"><a class="link" href="<?= $author_whatsapp ?>">WhatsApp</a></li>
+            <li class="item"><a class="link" href="<?= $author_youtube ?>">Youtube</a></li>
+            <li class="item"><a class="link" href="<?= $author_facebook ?>">Facebook</a></li>
+            <li class="item"><a class="link" href="<?= $author_instagram ?>">Instagram</a></li>
+            <li class="item"><a class="link" href="<?= $author_pinterest ?>">Pinterest</a></li>
           </ul>
         <dd>
       </div>
       <div class="column">
-        <dt>Contact Me</dt>
+        <dt>Contact Me<span class="underline"></span></dt>
         <dd>
           <form action="contact" method="POST">
             <input class="entry" type="email" name="address" placeholder="Type your address" required/>
