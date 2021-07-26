6dd6aff5eab2659d251eafe3ecde6d4b66ca5485
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-27 00:06:59 +0200

    Add server services

--- server/lib/Start/CGI/Server.php
+++ server/lib/Start/CGI/Server.php
@@ -6,25 +6,30 @@ class Server extends WebServer {
 
   protected array $menu = [];
 
-  public function onGet(callable $action): self {
+  public function onGet(string $action, callable $service): self {
     return $this->request('GET', $action);
   }
 
-  public function onPost(callable $action): self {
+  public function onPost(string $action, callable $service): self {
     return $this->request('POST', $action);
   }
 
   /**
    * On request server
    *
-   * @param  string   $method The request method
-   * @param  callable $action The request action
+   * @param  string   $method  The request method
+   * @param  string   $action  The request action
+   * @param  callable $service The request service
    * @return self
    */
-  public function onRequest(string $method, callable $action): self {
-    $method = strtoupper(trim($method));
-    $this->menu[$method] = $action;
+  public function onRequest(string $method, string $action, callable $service): self {
+    $method = $this->sanitizeMethod($method);
+    $this->menu[$method][$action] = $service;
     return $this;
+  }
+
+  protected function sanitizeMethod(string $method): string {
+    return strtoupper(trim($method));
   }
 
   /**
@@ -32,5 +37,19 @@ class Server extends WebServer {
    */
   public function menu(): array {
     return $this->menu;
+  }
+
+  /**
+   * Serve a client request
+   *
+   * @param  string $method The request method
+   * @param  string $action The request action
+   * @return void
+   */
+  public function serve(string $method, string $action): void {
+    $method = $this->sanitizeMethod($method);
+    if(!empty($services = $this->menu[$method]))
+      foreach($services as $action => $service)
+        //...
   }
 }
