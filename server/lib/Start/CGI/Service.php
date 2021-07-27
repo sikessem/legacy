1ac0eb337115b586c83a97b2a68dfe4dc8c5a02c
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-27 05:32:07 +0200

    Separate general and special methods and properties

--- server/lib/Start/CGI/Service.php
+++ server/lib/Start/CGI/Service.php
@@ -9,8 +9,6 @@ class Service extends WebService {
     parent::__construct($name, $callback);
   }
 
-  protected array $constraints = [];
-
   public function pattern(): string {
     return $this->pattern;
   }
@@ -29,68 +27,7 @@ class Service extends WebService {
     return true;
   }
 
-  protected array $options = [];
-
-  public function setOptions(array $options): self {
-    foreach($options as $key => $value)
-      $this->setOption($key, $value);
-    return $this;
-  }
-
-  public function setOption(string $key, mixed $value): self {
-    $this->options[$key] = $value;
-    return $this;
-  }
-
-  public function getOptions(array $keys = []): array {
-    if(empty($keys))
-      return $this->options;
-
-    $options = [];
-    foreach($keys as $key)
-      if($option = $this->getOption($key))
-        $options[$key] = $option;
-    return $options;
-  }
-
-  public function getOption(string $key): mixed {
-    return $this->options[$key] ?? null;
-  }
-
-  public function args(): array {
-    $args = [];
-    foreach($this->options as $key => $value)
-      if(is_int($key))
-        $args[$key] = $value;
-    return $args;
-  }
-
-  public function arg(string $key): mixed {
-    return $this->args()[$key] ?? null;
-  }
-
-  public function params(): array {
-    $params = [];
-    foreach($this->options as $key => $value)
-      if(is_string($key))
-        $params[$key] = $value;
-    return $params;
-  }
-
-  public function param(string $key): mixed {
-    return $this->params()[$key] ?? null;
-  }
-
   protected function matchKey(array $matches) {
     return isset($this->constraints[$matches[1]])? "(?<{$matches[1]}>{$this->constraints[$matches[1]]})": '(?<$1>[^\/]+)';
-  }
-
-  public function call(): mixed {
-    return ($this->bindCallback() ?? $this->callback)(...$this->args());
-  }
-
-  public function with(string $key, string $pattern): self {
-    $this->constraints[$key] = str_replace('(', '(?:', $pattern);
-    return $this;
   }
 }
--- server/lib/Start/Service.php
+++ server/lib/Start/Service.php
@@ -11,6 +11,8 @@ abstract class Service {
 
   protected string $name;
 
+  protected array $constraints = [];
+
   public function name(): string {
     return $this->name;
   }
@@ -23,9 +25,66 @@ abstract class Service {
 
   abstract public function match(string $pattern): bool;
 
-  abstract public function call(): mixed;
+  protected array $options = [];
 
-  abstract public function with(string $key, string $pattern): self;
+  public function setOptions(array $options): self {
+    foreach($options as $key => $value)
+      $this->setOption($key, $value);
+    return $this;
+  }
+
+  public function setOption(string $key, mixed $value): self {
+    $this->options[$key] = $value;
+    return $this;
+  }
+
+  public function getOptions(array $keys = []): array {
+    if(empty($keys))
+      return $this->options;
+
+    $options = [];
+    foreach($keys as $key)
+      if($option = $this->getOption($key))
+        $options[$key] = $option;
+    return $options;
+  }
+
+  public function getOption(string $key): mixed {
+    return $this->options[$key] ?? null;
+  }
+
+  public function args(): array {
+    $args = [];
+    foreach($this->options as $key => $value)
+      if(is_int($key))
+        $args[$key] = $value;
+    return $args;
+  }
+
+  public function arg(string $key): mixed {
+    return $this->args()[$key] ?? null;
+  }
+
+  public function params(): array {
+    $params = [];
+    foreach($this->options as $key => $value)
+      if(is_string($key))
+        $params[$key] = $value;
+    return $params;
+  }
+
+  public function param(string $key): mixed {
+    return $this->params()[$key] ?? null;
+  }
+
+  public function call(): mixed {
+    return ($this->bindCallback() ?? $this->callback)(...$this->args());
+  }
+
+  public function with(string $key, string $pattern): self {
+    $this->constraints[$key] = str_replace('(', '(?:', $pattern);
+    return $this;
+  }
 
   protected function bindCallback(): ?Closure {
     return $this->callbackIsBindable()? $this->callback->bindTo($this, $this::class): null;
