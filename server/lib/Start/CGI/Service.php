4a20417d5c785679c2bca315b2b81be6e126c7ab
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-27 02:14:03 +0200

    Manage the service matching

--- server/lib/Start/CGI/Service.php
+++ server/lib/Start/CGI/Service.php
@@ -4,16 +4,28 @@ use \Start\Service as WebService;
 
 class Service extends WebService {
 
-  public function __construct(string $path, string $name, callable $callback) {
-    $this->path = $path;
+  public function __construct(string $pattern, string $name, callable $callback) {
+    $this->pattern = $pattern;
     parent::__construct($name, $callback);
   }
 
-  public function path(): string {
-    return $this->path;
+  protected array $matches = [];
+
+  public function pattern(): string {
+    return $this->pattern;
   }
 
-  public function match(string $pattern): bool {
+  public function match(string $path): bool {
+    $path = trim($path, '/');
+    $pattern = preg_replace('/:([\w]+)/', '(?<$1>[^\/]+)', $this->pattern);
+    $pattern = "/^$pattern$/i";
+
+    if(!preg_match($pattern, $path, $matches))
+      return false;
+
+    array_shift($matches);
+    $this->matches = $matches;
+
     return true;
   }
 
