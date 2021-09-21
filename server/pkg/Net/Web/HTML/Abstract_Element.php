6102943c5ee1a2cfb27f407e5c58645260e64996
SIGUI Kessé Emmanuel (https://sikessem.com) <developer@sikessem.com>
2021-09-21 12:53:52 +0200

    :bug: fix(package): add array support from the HTML element component abstract class

--- server/pkg/Net/Web/HTML/Abstract_Element.php
+++ server/pkg/Net/Web/HTML/Abstract_Element.php
@@ -2,7 +2,7 @@
 namespace SIKessEm\Net\Web\HTML;
 
 abstract class Abstract_Element extends Abstract_ComponentName implements Element_Interface {
-  public function __construct(string $name, array|Attribute_Interface $attributes = [], null|string|Element_Interface $content = null) {
+  public function __construct(string $name, array|Attribute_Interface $attributes = [], null|string|Element_Interface|array $content = null) {
     parent::__construct($name);
     $this->setAttributes($attributes);
     $this->setContent($content);
