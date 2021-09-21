2c81f12e61bdc97987dbf01bc103e2a20f6a8cbb
SIGUI Kessé Emmanuel (https://sikessem.com) <developer@sikessem.com>
2021-09-21 08:48:07 +0200

    :art: chore(template): use HTML attribute components

--- server/tpl/home.view.php
+++ server/tpl/home.view.php
@@ -1,15 +1,21 @@
-<main class="home">
-  <div class="heading">
+<?php
+use function SIKessEm\Net\Web\HTML\{
+  attributes,
+  className
+};
+?>
+<main <?= className('home') ?>>
+  <div <?= className('heading') ?>>
     <h1>Get your appliance cheaply and on time</h1>
   </div>
-  <div class="content">
-    <div class="subtitle">
+  <div <?= className('content') ?>>
+    <div <?= className('subtitle') ?>>
       <p><?= $app_author ?> is a self-taught developer of websites, smartphone apps and cross-plateform software.</p>
     </div>
-    <div class="items">
-      <ul class="buttons list w box centered sa">
-        <li class="item"><a href="https://omninf.com/SIKessEm/profile/" class="button dark" title="All about <?= $author_fullname?>">All about <?= $app_name?></a></li>
-        <li class="item"><a href="https://omninf.com/SIKessEm/contact/" class="button light" title="Contact <?= $author_fullname?>">Contact <?= $app_name?></a></li>
+    <div <?= className('items') ?>>
+      <ul <?= className('buttons list w box centered sa') ?>>
+        <li <?= className('item') ?>><a <?= attributes(['href' => 'https://omninf.com/SIKessEm/profile/', 'class' => 'button dark', 'title' => "All about $author_fullname"]) ?>>All about <?= $app_name?></a></li>
+        <li <?= className('item') ?>><a <?= attributes(['href' => 'https://omninf.com/SIKessEm/contact/', 'class' => 'button light', 'title' => "Contact $author_fullname"]) ?>>Contact <?= $app_name?></a></li>
       </ul>
     </div>
   </div>
--- server/tpl/main.header.php
+++ server/tpl/main.header.php
@@ -1,3 +1,9 @@
-<header class="header">
-  <h1><a href="https://omninf.com/SIKessEm/" class="logo" title="<?= $app_name ?> home"><?= $app_name ?></a></h1>
+<?php
+use function SIKessEm\Net\Web\HTML\{
+  attributes,
+  className
+};
+?>
+<header <?= className('header')?>>
+  <h1><a <?= attributes(['href' => 'https://omninf.com/SIKessEm/', 'class' => 'logo', 'title' => $app_name . 'home']) ?>><?= $app_name ?></a></h1>
 </header>
