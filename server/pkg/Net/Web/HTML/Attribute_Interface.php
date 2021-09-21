cd3e973cc4a52c18209d21061cd21340658303e6
SIGUI Kessé Emmanuel (https://sikessem.com) <developer@sikessem.com>
2021-09-21 09:04:37 +0200

    :art: chore(package): create the HTML name component and use it in the HTML attribute component

--- server/pkg/Net/Web/HTML/Attribute_Interface.php
+++ server/pkg/Net/Web/HTML/Attribute_Interface.php
@@ -1,9 +1,7 @@
 <?php
 namespace SIKessEm\Net\Web\HTML;
 
-interface Attribute_Interface extends Component_Interface {
-  public function is(string $name): bool;
-  public function getName(): string;
+interface Attribute_Interface extends Component_Interface, Name_Interface {
   public function setValue(string $value): static;
   public function getValue(): string;
 }
--- server/pkg/Net/Web/HTML/Attribute_Trait.php
+++ server/pkg/Net/Web/HTML/Attribute_Trait.php
@@ -2,24 +2,7 @@
 namespace SIKessEm\Net\Web\HTML;
 
 trait Attribute_Trait {
-
-  protected string $name;
-
-  protected function setName(string $name): static {
-    $name = Filter::sanitize($name);
-    if (!Filter::validate($name))
-      throw new Error("Invalid attribute name give", Error::INVALID_NAME);
-    $this->name = $name;
-    return $this;
-  }
-
-  public function getName(): string {
-    return $this->name;
-  }
-
-  public function is(string $name): bool {
-	return $this->name === Filter::sanitize($name);
-  }
+  use Name_Trait;
 
   protected string $value;
 
