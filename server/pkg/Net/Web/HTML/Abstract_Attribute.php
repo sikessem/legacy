5ad99e5e08ac4b0c7c68c420bc9f9d9bb6b17bed
SIGUI Kessé Emmanuel (https://sikessem.com) <developer@sikessem.com>
2021-09-22 10:55:33 +0200

    :recycle: refactor(package): use the HTML node component from HTML atribute and HTML element components

--- server/pkg/Net/Web/HTML/Abstract_Attribute.php
+++ server/pkg/Net/Web/HTML/Abstract_Attribute.php
@@ -1,10 +1,8 @@
 <?php
 namespace SIKessEm\Net\Web\HTML;
 
-abstract class Abstract_Attribute implements Attribute_Interface {
+abstract class Abstract_Attribute extends Abstract_NodeName implements Attribute_Interface {
   public function __construct(string $name, string $value) {
     $this->setName($name)->setValue($value);
   }
-
-  abstract protected function setName(string $name): static;
 }
