3a1254beff3f5282446239fca73a7dd784b80d7d
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-11-17 07:52:38 +0100

    :heavy_plus_sign: Add Response to the RequestContext

--- packages/core/src/RequestContext.php
+++ packages/core/src/RequestContext.php
@@ -7,18 +7,21 @@ namespace Sikessem;
 class RequestContext extends Context
 {
     protected Request $request;
+    protected Response $response;
 
-    public function __construct(Request $request, Environment $environment)
+    public function __construct(Request $request, Response $response, Environment $environment)
     {
         parent::__construct($environment);
         $this->setRequest($request);
+        $this->setResponse($response);
     }
 
     public static function fromGlobals(): self
     {
         $request = Request::fromGlobals();
+        $response = Response::fromGlobals();
         $environment = Environment::fromGlobals();
-        return new self($request, $environment);
+        return new self($request, $response, $environment);
     }
 
     public function getRequest(): Request
@@ -29,6 +32,18 @@ class RequestContext extends Context
     public function setRequest(Request $request): self
     {
         $this->request = $request;
+
+        return $this;
+    }
+
+    public function getResponse(): Response
+    {
+        return $this->response;
+    }
+
+    public function setResponse(Response $response): self
+    {
+        $this->response = $response;
 
         return $this;
     }
